54acd62c81f3c54260262cc60909f8f6c750bdc5
joy gajjar <gajjarjoy4@gmail.com>
2024-06-28 14:15:50 +0530

    Now the project can be deployed to tomcat

--- webApp/src/main/java/com/joy/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.joy.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.joy.entities.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
